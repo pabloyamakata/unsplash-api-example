5e9d71327298a9f86cce3d12875bfed4dd3d3de5
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-20 17:21:27 -0300

    feat: create loop to iterate api call results

--- script.js
+++ script.js
@@ -7,7 +7,17 @@ const accessKey = config.ACCESS_KEY;
 const fetchData = async() => {
     const response = await fetch(`${url}/search/photos?query=${query}&client_id=${accessKey}`);
     const data = await response.json();
-    root.innerHTML = data.results[0].alt_description;
+
+    let gridItems = [];
+    let fragment = document.createDocumentFragment();
+
+    for(i = 0; i < data.results.length; i++) {
+        gridItems[i] = document.createElement('div');
+        gridItems[i].style.backgroundImage = `url(${data.results[i].urls.raw})`;
+        fragment.appendChild(gridItems[i]);
+    }
+
+    root.appendChild(fragment);
 };
 
 fetchData();
