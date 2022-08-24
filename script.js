8383f356e5b0025973501ed93fa19e2223b9319c
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-23 21:55:26 -0300

    feat: use Intersection Observer API to lazy load images

--- script.js
+++ script.js
@@ -4,12 +4,32 @@ const input = document.getElementById('input');
 const url = 'https://api.unsplash.com';
 const accessKey = config.ACCESS_KEY;
 
+let urls = [];
+
+// Intersection Observer API
+const options = {
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.6
+};
+
+let callback = (entries, observer) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting) {
+            entry.target.style.backgroundImage = `url(${urls[0]})`;
+            observer.unobserve(entry.target);
+        }
+    });
+};
+
+let observer = new IntersectionObserver(callback, options);
+
 const fetchData = async() => {
     root.replaceChildren();
 
     if(input.value == '') input.value = 'London';
 
-    const response = await fetch(`${url}/search/photos?query=${input.value}&client_id=${accessKey}`);
+    const response = await fetch(`${url}/search/photos?query=${input.value}&per_page=30&client_id=${accessKey}`);
     const data = await response.json();
 
     if(data.results.length > 0) {
@@ -18,7 +38,8 @@ const fetchData = async() => {
 
         for(i = 0; i < data.results.length; i++) {
             images[i] = document.createElement('div');
-            images[i].style.backgroundImage = `url(${data.results[i].urls.raw})`;
+            images[i].setAttribute('class', 'image');
+            urls[i] = data.results[i].urls.raw;
     
             if(data.results[i].width > data.results[i].height) {
                 images[i].classList.add('column-span');
@@ -30,6 +51,11 @@ const fetchData = async() => {
         }
 
         root.appendChild(fragment);
+        let targets = document.querySelectorAll('.image');
+        
+        targets.forEach(target => {
+            observer.observe(target);
+        });
     } else {
         alert('0 results!');
     }
