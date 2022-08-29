835d3cb34ac6483cff40a6d4d5996899433da264
Pablo Yamakata <pablo.yamakata@gmail.com>
2022-08-28 21:38:20 -0300

    style: make modals responsive

--- modules/getRandomItem.js
+++
@@ -1,8 +0,0 @@
-const getRandomItem = array => {
-    let index = Math.floor(Math.random() * (array.length));
-    let item = array[index];
-    array.splice(index, 1);
-    return item;
-};
-
-export default getRandomItem;
